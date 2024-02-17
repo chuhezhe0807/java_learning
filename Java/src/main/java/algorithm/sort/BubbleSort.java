2e8913b90fd698145766cfa6f143240ed370ef32
chuhezhe <chuhezhe0807@163.com>
2024-02-17 22:34:47 +0800

    learning: 冒泡排序优化;

--- Java/src/main/java/algorithm/sort/BubbleSort.java
+++ Java/src/main/java/algorithm/sort/BubbleSort.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
  */
 public class BubbleSort {
     public static void main(String[] args) {
-        bubble(new int[]{4, 2, 1, 4, 5, 3});
+        bubble0(new int[]{4, 2, 1, 4, 5, 3});
     }
 
     public static void bubble(int[] arr) {
@@ -31,6 +31,31 @@ public class BubbleSort {
                     int temp = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // 如果某次遍历中没有发生交换，那么就不必进行下一次遍历因为所有元素都已经排好序了。
+    public static void bubble0(int[] arr) {
+        if(arr == null || arr.length < 2) {
+            return;
+        }
+
+        boolean needNextPass = true;
+
+        for(int i = 1; i < arr.length && needNextPass; i++) {
+            needNextPass = false;
+
+            for(int j = 0; j < arr.length - i; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    needNextPass = true;
                 }
             }
         }
