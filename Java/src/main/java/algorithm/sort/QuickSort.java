4656eb6e3a0ed166b1d280912bc7701d37c5fad4
chuhezhe <chuhezhe0807@163.com>
2024-02-18 23:09:06 +0800

    learning: 快速排序再理解;

--- Java/src/main/java/algorithm/sort/QuickSort.java
+++ Java/src/main/java/algorithm/sort/QuickSort.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 /**
  * 快速排序
- *  时间复杂度
+ *  时间复杂度 O(nlogn) 符合master公式应用要求 T(N) = 2*T(N/2) + N
+ *  工作机制：在数组中选择一个称为基准的元素，将数组分为两部分，使得第一部分中的所有元素都小于等于基准元素
+ *              而第二部分中的元素都大于等于基准元素。对这两部分都递归的应用快速排序。
  *
  * ClassName: QuickSort
  * Package: algorithm.sort
@@ -32,6 +34,8 @@ public class QuickSort {
     // arr[l...r] 排序
     public static void quickSort(int[] arr, int l, int r) {
         if(l < r) {
+            // 将数组最后一个值与arr数组中任意一个元素交换位置，就是随机选择一个值为划分值arr[r]
+            // 注释掉下面这一行就是默认以arr[r]做划分
             swap(arr, l + (int) (Math.random() * (r - l + 1)), r);
             int[] p = partition(arr, l, r);
             quickSort(arr, l, p[0] - 1); // < 区
@@ -41,16 +45,19 @@ public class QuickSort {
 
     // 这是一个处理arr[l..r]的函数
     // 默认以arr[r]做划分，arr[r] -> p       <p   ==p  >p
+    // arr数组分为arr[r]和其他的区域，其他的区域又被分为两个区域，小于arr[r]的放在左边，大于的放在右边
+    // 然后arr[r]和大于区域的左边界做交换，相当于说小于arr[r]区域扩充了，小于区域的最后一个数一定是arr[r]
+    // 此时整个数组中arr[r]这个数就可以认为已经排好序了，大于区域和小于区域再重复这一操作，就可以让整个数组排好序了。
     // 返回等于区域（左边界，右边界），所以返回一个长度为2的数组res
     private static int[] partition(int[] arr, int l, int r) {
-        int less = l - 1; // < 区右边界
-        int more = r; // > 区左边界
+        int less = l - 1; // 小于arr[r]区域右边界
+        int more = r; // 大于arr[r]区域左边界
 
-        while(l < more) { // l标识当前数的位置 arr[r] -> 划分值
-            if(arr[l] < arr[r]) { // 当前数 < 划分值
+        while(l < more) { // l标识当前数的位置 arr[r] -> 划分值  while循环的限制条件为小于arr[r]区域的右边界一定小于大于arr[r]区域的左边界
+            if(arr[l] < arr[r]) { // 当前数 < 划分值，小于arr[r]区域的右边界向右扩充，并交换当前值和小于区域的右边界值，当前值对应的索引++
                 swap(arr, ++less, l++);
             }
-            else if(arr[l] > arr[r]) { // 当前数 > 划分值
+            else if(arr[l] > arr[r]) { // 当前数 > 划分值，大于arr[r]区域的左边界向左扩充，并交换当前值和大于区域的右边界值
                 swap(arr, --more, l);
             }
             else {
@@ -58,7 +65,7 @@ public class QuickSort {
             }
         }
 
-        swap(arr, more, r);
+        swap(arr, more, r); // arr[r]和大于区域的左边界交换位置
 
         return new int[]{less + 1, more};
     }
